cb37f1d43fae2bf68450568bf311660aa8f4867c
rathourmonu274 <rathourmonu274@gmail.com>
2026-06-02 11:04:46 +0530

    Day 4 operators and simple calculator done

---
+++ DAY 4/operator.py
@@ -0,0 +1,10 @@
+# Arithmatic operations
+
+# calculator usinng arithmatic operation
+
+a = 10
+b = 7
+print("the addition of a and b = ", a+b)
+print("the subtraction of a and b = ", a-b)
+print("the multiplication of a and b = ", a*b)
+print("the division of a and b = ", a/b)
