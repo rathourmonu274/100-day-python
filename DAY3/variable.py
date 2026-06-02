53cc62b76d7b91568a967c91d41a950c42e1adee
rathourmonu274 <rathourmonu274@gmail.com>
2026-06-02 10:46:55 +0530

    Day 3 variale and data type complete

---
+++ DAY3/variable.py
@@ -0,0 +1,19 @@
+# A variable is a container that stores data.
+
+name = "Monu"
+age = 20
+print(name)
+print(age)
+
+
+# Data Types
+
+name = "Monu"
+age = 20
+height = 5.8
+is_student = True
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
